bdab5fc48bb109c2697c8f01bcbc734cbdc34b38
Dave Dunkin <dave.dunkin@logos.com>
2022-06-02 12:47:06 -0700

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+if (args.Length != 1)
+{
+	Console.Error.WriteLine("Usage: dumpe assembly.dll");
+	return 1;
+}
+
+var filePath = args[0];
+try
+{
+	File.OpenHandle(filePath);
+}
+catch (FileNotFoundException)
+{
+	Console.Error.WriteLine("File not found");
+	return 1;
+}
+catch (UnauthorizedAccessException)
+{
+	Console.Error.WriteLine("Permission denied");
+	return 1;
+}
+catch (IOException ex)
+{
+	Console.Error.WriteLine($"Cannot open file: {ex}");
+	return 1;
+}
+
+var versionInfo = FileVersionInfo.GetVersionInfo(filePath);
+Console.WriteLine(versionInfo);
+
+return 0;
